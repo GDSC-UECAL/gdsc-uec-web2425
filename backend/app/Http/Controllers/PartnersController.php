2b6b525d5ec8bcdfd40522f32a03a6d50df5ab91
quaamy <zalzossebastian@gmail.com>
2024-09-28 15:10:23 +0800

    Update tables for Partners and Carousel

---
+++ backend/app/Http/Controllers/PartnersController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Partners;
+use App\Http\Requests\StorePartnersRequest;
+use App\Http\Requests\UpdatePartnersRequest;
+
+class PartnersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePartnersRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Partners $partners)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Partners $partners)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePartnersRequest $request, Partners $partners)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Partners $partners)
+    {
+        //
+    }
+}
--- backend/database/migrations/2024_09_28_070132_create_carousels_table.php
+++ backend/database/migrations/2024_09_28_070132_create_carousels_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('carousels', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->char('image');
+            $table->string('image');
             $table->timestamps();
         });
     }
