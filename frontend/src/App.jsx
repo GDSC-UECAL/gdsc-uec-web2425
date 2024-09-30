d073e221a9a96e3c6c73a7c766f5a71ae8b39fed
tacosNburritos <renzoisaac912@gmail.com>
2024-09-30 23:05:01 +0800

    Semi-customized Navbar
    Added padding, font color, hover elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/NavBar.jsx'
 import './App.css'
-
+import './components/Navbar.css'
 function App() {
   const [count, setCount] = useState(0)
 
