e0eae408033ecd8312f11f42a851bc898f732ac2
Zyldjan Lanticse <115306019+Zyd8@users.noreply.github.com>
2024-10-01 18:31:55 +0800

    fix inconsistent model name: Events

---
+++ backend/app/Http/Controllers/EventsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEventsRequest;
+use App\Http\Requests\UpdateEventsRequest;
+use App\Models\Events;
+
+class EventsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEventsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Events $events)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Events $events)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEventsRequest $request, Events $events)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Events $events)
+    {
+        //
+    }
+}
