18a09c097c006bd4b8212d92d2ec881e9ffc17f1
tacosNburritos <renzoisaac912@gmail.com>
2024-10-01 21:22:01 +0800

    Temporary Footer
    Created a temporary footer for the mean  time. Still needs proper text alignment, and GDSC logo. Sending this just so there could be an uploaded copy of this work.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import Navbar from './components/NavBar.jsx'
 import './App.css'
 import './components/Navbar.css'
+import Footer from "./components/Footer.jsx"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Navbar></Navbar>
+    <Navbar/>
+    <Footer/>
+
     </>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,52 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer className="bg-dark text-light mt-auto py-3 fixed-bottom">
+    <div className="container-fluid">
+        <div className="row text-center">
+            <div className="col-12 col-md-6 mb-2">
+                <h6>Google Developer Student Clubs</h6>
+                <p>University of the East - Caloocan</p>
+                <p>Together, we discover future.</p>
+                <p>105 Samson Road, Caloocan City, 1408</p>
+                <p>Need Help? Contact: gdsc.ue@gmail.com</p>
+            </div>
+            <div className="col-12 col-md-1 mb-2">
+                <h6>Home</h6>
+                <a href="">Welcome</a> <br />
+                <a href="">Bulletin</a> <br />
+                <a href="">Partners</a>
+            </div>
+            <div className="col-12 col-md-1 mb-2">
+                <h6>About</h6>
+                <a href="">Welcome</a> <br />
+                <a href="">Bulletin</a> <br />
+                <a href="">Partners</a>
+            </div>
+            <div className="col-12 col-md-1 mb-2">
+                <h6>Products</h6>
+                <a href="">Welcome</a> <br />
+                <a href="">Bulletin</a> <br />
+                <a href="">Partners</a>
+            </div>
+            <div className="col-12 col-md-1 mb-2">
+                <h6>Team</h6>
+                <a href="">Welcome</a> <br />
+                <a href="">Bulletin</a> <br />
+                <a href="">Partners</a>
+            </div>
+            <div className="col-12 col-md-1 mb-2">
+                <h6>Events</h6>
+                <a href="">Welcome</a> <br />
+                <a href="">Bulletin</a> <br />
+                <a href="">Partners</a>
+            </div>
+        </div>
+    </div>
+</footer>
+
+    )
+}
+
+export default Footer
