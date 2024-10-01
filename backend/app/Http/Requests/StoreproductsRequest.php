7f4bed5ce6a1e67a44bee1228b781d8abd9565cf
RANEASHBELL <kennyranes2319@gmail.com>
2024-10-01 17:08:53 +0800

    remake backend for Products
    remake backend for Products

--- backend/app/Http/Requests/StoreproductsRequest.php
+++ backend/app/Http/Requests/StoreproductsRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreproductsRequest extends FormRequest
+class StoreProductsRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
