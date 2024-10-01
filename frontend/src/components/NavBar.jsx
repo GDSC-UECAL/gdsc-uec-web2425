9343622acf86b2feef8b7bf90dc1d08fe43b7476
Xynpse <zac.bayten@gmail.com>
2024-10-01 21:03:57 +0800

    Nav bar update
    Hindi pa naka kabit mga pages. Subject to change, Register button needs to be added. Renzo Mendoza, change what needs to be changed. tyty

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,34 +1,39 @@
+import "../assets/common/gdsc_logo.png"
+import nav_logo from "../assets/common/gdsc_logo.png"
+
 
 function Navbar() {
     return (
         <>
-            <nav class="navbar navbar-expand-lg fixed-top">
-                <div class="container-fluid">
-                    <a class="navbar-brand mx-lg-2" href="#">LOGO</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg fixed-top">
+                <div className="container-fluid">
+                    <a className="navbar-brand mx-lg-2" href="#">
+                    <img src={nav_logo} alt="Logo" />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
-                        <div class="offcanvas-body">
-                            <ul class="navbar-nav justify-content-first flex-grow-1 pe-3">
-                                <li class="nav-item">
-                                    <a class="nav-link mx-lg-2 active" aria-current="page" href="#">Home</a>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-first flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                    <a className="nav-link mx-lg-2" aria-current="page" href="#">Home</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link mx-lg-2" href="#">Events</a>
+                                <li className="nav-item">
+                                    <a className="nav-link mx-lg-2" href="#">About</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link mx-lg-2" href="#">Product</a>
+                                <li className="nav-item">
+                                    <a className="nav-link mx-lg-2" href="#">Product</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link mx-lg-2" href="#">Teams</a>
+                                <li className="nav-item">
+                                    <a className="nav-link mx-lg-2" href="#">Teams</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link mx-lg-2" href="#">About</a>
+                                <li className="nav-item">
+                                    <a className="nav-link mx-lg-2" href="#">Events</a>
                                 </li>
                             </ul>
                         </div>
@@ -39,4 +44,4 @@ function Navbar() {
     )
 }
 
-export default Navbar
+export default Navbar;
