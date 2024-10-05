244a452814dd3be7d854cf23490a373399f1343b
keni <kennyranes2319@gmail.com>
2024-10-05 14:19:48 +0800

    created Merchandises table
    created Merchandises table

---
+++ backend/app/Http/Controllers/MerchandisesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Merchandises;
+use App\Http\Requests\StoreMerchandisesRequest;
+use App\Http\Requests\UpdateMerchandisesRequest;
+
+class MerchandisesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMerchandisesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Merchandises $merchandises)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Merchandises $merchandises)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMerchandisesRequest $request, Merchandises $merchandises)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Merchandises $merchandises)
+    {
+        //
+    }
+}
