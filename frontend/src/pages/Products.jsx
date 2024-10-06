a5bc5f578057a0d9eaf9925f1ce31b9c7a1539a7
sum1sum <agullana.samuel21@gmail.com>
2024-10-06 12:50:35 +0800

    imported products.css to products.jsx

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -3,6 +3,7 @@ import ProductsCarousel from '../components/ProductsCarousel.jsx';
 import "../components/ProductsCarousel.css"
 import Merchandises from '../components/Merchandises.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap is imported
+import "../pages/Products.css"
 
 
 
