3f18dbf3073c5ca0f2c9812c54e9bc83a1c522e1
quaamy <zalzossebastian@gmail.com>
2024-10-06 11:59:51 +0800

    Update names for departments

--- backend/database/migrations/2024_10_01_091726_create_department_table.php
+++ backend/database/migrations/2024_10_01_091726_create_department_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('department', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('description');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('department');
+        Schema::dropIfExists('departments');
     }
 };
