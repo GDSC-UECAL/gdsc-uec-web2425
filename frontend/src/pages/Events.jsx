ce325ca611b72cf2826504d69e4fb2f7b1d278bd
Jul Romeral <jkromeral31@gmail.com>
2024-10-07 16:05:27 +0800

    added images in events page

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import "./Events.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import cube from '../assets/common/6.png';
+import ball from "../assets/common/1.png";
+import vector1 from '../assets/common/vector_c.png';
+import vector2 from '../assets/common/vector_f.png';
 
 function Events() {
     const [events, setEvents] = useState([]);
@@ -20,23 +24,29 @@ function Events() {
     }
 
     return (
-        <main id="event-main">
-            <h1>Events</h1>
-            <div id="event-container">
-                {(showAllEvents ? events : events.slice(0, 4)).map((event) => (
-                    <div key={event.id}>
-                        <img src={event.banner} alt={event.banner} />
-                        <h6>{event.title}</h6>
-                        <p>{event.date}</p>
-                    </div>
-                ))}
-            </div>
-            {!showAllEvents && (
-                <div id="event-button">
-                    <button className="btn btn-primary text-white" onClick={handleShowAll}>See More</button>
+        <>
+            <img className="event-image cube" src={cube} alt="cube"/>
+            <img className="event-image ball" src={ball} alt="ball"/>
+            <img className="event-image vector1" src={vector1} alt="vector1"/>
+            <img className="event-image vector2" src={vector2} alt="vector2"/>
+            <main id="event-main">
+                <h1>Events</h1>
+                <div id="event-container">
+                    {(showAllEvents ? events : events.slice(0, 4)).map((event) => (
+                        <div key={event.id}>
+                            <img src={event.banner} alt={event.banner} />
+                            <h6>{event.title}</h6>
+                            <p>{event.date}</p>
+                        </div>
+                    ))}
                 </div>
-            )}
-        </main>
+                {!showAllEvents && (
+                    <div id="event-button">
+                        <button className="btn btn-primary text-white" onClick={handleShowAll}>See More</button>
+                    </div>
+                )}
+            </main>
+        </>
     )
 }
 
